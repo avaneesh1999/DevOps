8654d069b31e1cf84f59f9cca448c7b878f9e88d
Avaneesh <avaneeshsingh1999@gmail.com>
2022-10-21 16:25:11 +0530

    first commit

---
+++ GO/Booking-app/main.go
@@ -0,0 +1,14 @@
+package main
+
+
+import "fmt"
+
+func main(){
+	var confrenceName= "PVR"
+	const Totaltickets=50
+	var RemainingTickets=50
+
+	fmt.Printf("Welcome to %v \n",confrenceName);
+	fmt.Printf("Get your Tickets here \n");
+	fmt.Printf("there are Total %v Tickets and %v are available now hurry up!!! \n",Totaltickets,RemainingTickets,)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module Booking-app
+
+go 1.19
