6cb678fd47addc024e163246fefd99e944f53f11
Avaneesh <avaneeshsingh1999@gmail.com>
2022-10-22 21:23:51 +0530

    till for loop

--- GO/Booking-app/main.go
+++ GO/Booking-app/main.go
@@ -1,16 +1,24 @@
 package main
 
-
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main(){
+
 	var confrenceName= "PVR"
 	const Totaltickets=50
 	var RemainingTickets uint
 
+    //slices 
+	bookings:=[]string{}
+
 	fmt.Printf("Welcome to %v \n",confrenceName);
 	fmt.Printf("Get your Tickets here \n");
 	
+ for{
+
 
 	var firstName string
 	var lastName string
@@ -30,13 +38,19 @@ func main(){
 	fmt.Scan(&userTicket)
 
 	RemainingTickets=Totaltickets-userTicket
+	bookings=append(bookings,firstName+" "+lastName)
 
-	fmt.Printf(" Thankyou %v %v for Booking %v tickets  you will recieve a confirmation on your %v email address. \n",firstName,lastName,userTicket,email,);
+	
+     fmt.Printf(" Thankyou %v %v for Booking %v tickets  you will recieve a confirmation on your %v email address. \n",firstName,lastName,userTicket,email,);
     
 	fmt.Printf("There are Total %v Tickets and %v are available now hurry up!!! \n",Totaltickets,RemainingTickets,)
+	
+	fmt.Printf("These are all your bookiungs %v \n",bookings,)
 
-
-
-
-
+	// firstNames:=[]string{}
+	//   for_, booking := range bookings {
+	// 	var names = strings.Fields(booking)
+	// 	 firstNames = append(firstNames,names[0])
+	// }
+}
 }
